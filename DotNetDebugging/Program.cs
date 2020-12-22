18c226dccf01389ddc80a0d6d0a78e3bae73706f
Marcelo Pena Vila <marcelopena2010@gmail.com>
2020-12-22 14:16:18 -0300

    Criação dos projetos
    Adicionandos dos projetos nos cursos.

---
+++ DotNetDebugging/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DotNetDebugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Fibonacci(5);
+            Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+            Debug.WriteLine($"We are looking for the {n}th number");
+        }
+        static int Fibonacci(int n)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int sum = 0;
+
+            for (int i = 2; i < n; i++)
+            {
+                sum = n1 + n2;
+                n1 = n2;
+                n2 = sum;
+                Debug.WriteLineIf(sum == 1, $"sum is 1, n1 is {n1}, n2 is {n2}");
+            }
+            return n == 0 ? n1 : n2;
+        }
+    }
+}
