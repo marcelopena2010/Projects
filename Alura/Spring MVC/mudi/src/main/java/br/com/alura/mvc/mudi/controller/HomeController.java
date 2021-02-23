637e5e0e327bec78c9df41b0cbc08689c36bae6e
Marcelo Pena Vila <marcelopena2010@gmail.com>
2021-02-23 10:54:42 -0300

    Curso de Spring MVC
    Projeto sobre Spring MVC

--- Alura/Spring MVC/mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ Alura/Spring MVC/mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,29 +1,43 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.StatusPedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
-
-	@GetMapping("/home")
+	
+	@Autowired
+	private PedidoRepository repository;
+	
+	@GetMapping()
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Xiaomi Redmi Note 9S");
-		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/61ShPQu-u0L._AC_SL1200_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Smartphone-Xiaomi-Redmi-Note-9S/dp/B085S47W3M?ref_=Oct_s9_apbd_otopr_hd_bw_bHjJLCl&pf_rd_r=S5E7YV3VHNMC6WMTG8M2&pf_rd_p=1da659a0-948a-52ba-a216-243433981446&pf_rd_s=merchandised-search-10&pf_rd_t=BROWSE&pf_rd_i=16243803011");
-		pedido.setDescricao("uma descrição qualquer para esse pedido");
-		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		List<Pedido> pedidos = repository.findAll();
 		model.addAttribute("pedidos", pedidos);
-		
-		return "home";
+		return "home"; 
 	}
 	
+	@GetMapping("/{status}")
+	public String porStatus(@PathVariable("status") String status, Model model) {
+		List<Pedido> pedidos = repository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
+		model.addAttribute("pedidos", pedidos);
+		model.addAttribute("status", status);
+		return "home"; 
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	public String onError() {
+		return "redirect:/home";
+	}
 }
